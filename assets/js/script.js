6e664cc5cbf0da134be5bcdc2e6f77254b4bed74
Disha2022 <99584117+Disha2022@users.noreply.github.com>
2022-03-27 16:08:39 -0400

    creating timeblock

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,17 @@ var todaysDate= (moment().format('MMMM Do YYYY, h:mm:ss a'))
 var dateText= $("<p>").text(todaysDate) //making new p element
 $("#currentDay").replaceWith(dateText);
 //Add time, text and save (column can be adjusted using bootstrap)
+var timeStart = 8; //business hour start
+var timeEnd = 5; //business hour ends
+
+//Create time block
+for (let i = 0; i < 9; i++) {
+    const element = $("<input>")
+    .attr('id', 'to-do');
+    var timeGap = timestart+ i
+    var timeStamp = $("<p>").text(timeGap)
+    
+}
+$("#submit").click(function() {
+   $("#to-do").value
+});
